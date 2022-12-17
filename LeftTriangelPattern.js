99c819627ec7be4136931f0f051184fe5f02cafd
Snehal-Wohlig <snehal.kamble@wohlig.com>
2022-12-18 00:01:12 +0530

    Left Triangel pattern

---
+++ LeftTriangelPattern.js
@@ -0,0 +1,14 @@
+let n = 5;
+let string = "";
+for (let i = 1; i <= n; i++) {
+    console.log("external loop print i value",i);
+  for (let j = 0; j < i; j++) {
+    console.log("Internal loop print j value",j);
+    string += "*";
+    console.log("Internal loop print * value",string)
+  }
+  console.log("print @@@@ ");
+  string += "\n";
+  console.log("print new line",string)
+}
+console.log(string);
